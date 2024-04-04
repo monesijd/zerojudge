0d98ae9d7755e07829d469c92972e7f156a972dc
monesijd <140719255+monesijd@users.noreply.github.com>
2024-04-04 11:50:34 +0800

    Sovled f045.

---
+++ f045.py
@@ -0,0 +1,10 @@
+id = input()
+id_list = []
+for i in id:
+    id_list.append(int(i))
+id_number = int(id[-3:])
+sorted_id = sorted(id_list, reverse=True)
+if int(sorted_id[0]) ** 2 + int(sorted_id[1]) ** 2 == id_number:
+    print("Good Morning!")
+else:
+    print("SPY!")
