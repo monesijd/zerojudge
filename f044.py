7b843d06c979bb1e5afaad9017f65df7cf4b32d0
monesijd <140719255+monesijd@users.noreply.github.com>
2024-04-04 11:45:31 +0800

    Sovled f044.

---
+++ f044.py
@@ -0,0 +1,7 @@
+big_slime, little_slime = map(int, input().split())
+end_number = (big_slime + little_slime) // big_slime
+day = 1
+while end_number != 1:
+    end_number = end_number // 2
+    day += 1
+print(day-1)
