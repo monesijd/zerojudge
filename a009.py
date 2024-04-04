9b487c994942d7115d149b256243622cc11149b3
monesijd <140719255+monesijd@users.noreply.github.com>
2024-04-04 11:20:29 +0800

    Sovled a009.

---
+++ a009.py
@@ -0,0 +1,9 @@
+code = input().split()
+i = 0
+value = []
+while True:
+    value[0].append(code[0])
+    if value[0] + i == '*':       
+        for j in range(len(code)):        
+            print()
+    i +=1
