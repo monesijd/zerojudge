7c580107438270c6eda347a316dfc6dc1f92757b
monesijd <140719255+monesijd@users.noreply.github.com>
2024-04-04 11:46:28 +0800

    Sovled e975.

---
+++ e975.py
@@ -0,0 +1,11 @@
+password = input().lower()
+l = ord('l')
+o = ord('o')
+v = ord('v')
+e = ord('e')
+for i in range(len(password)-3):
+    if (ord(password[i]) - ord(password[i+1])) % 26 == (l-o) % 26:
+        if (ord(password[i+1]) - ord(password[i+2])) % 26 == (o-v) % 26:
+            if (ord(password[i+2]) - ord(password[i+3])) % 26 == (v-e) % 26:
+                print((l - ord(password[i])) % 26)
+                break   
