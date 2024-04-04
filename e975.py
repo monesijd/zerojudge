a7a32b31b4c1dd0e484d18cbd0fca8ecb2370529
monesijd <140719255+monesijd@users.noreply.github.com>
2024-04-04 13:18:16 +0800

    Solved e975.

--- e975.py
+++ e975.py
@@ -8,4 +8,4 @@ for i in range(len(password)-3):
         if (ord(password[i+1]) - ord(password[i+2])) % 26 == (o-v) % 26:
             if (ord(password[i+2]) - ord(password[i+3])) % 26 == (v-e) % 26:
                 print((l - ord(password[i])) % 26)
-                break   
+                break    
