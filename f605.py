3902905515eb0e58acf7c4737677369d621b0b97
monesijd <140719255+monesijd@users.noreply.github.com>
2024-04-04 13:04:23 +0800

    Sovled f605.

---
+++ f605.py
@@ -0,0 +1,14 @@
+number, d = map(int, input().split())
+buy_number = 0
+buy_money = 0
+
+for _ in range(number):
+    price = list(map(int, input().split()))
+    average = int((price[0] + price[1] + price[2]) / 3)
+    sorted_price = sorted(price)
+    if sorted_price[2] - sorted_price[0] >= d:
+        buy_number += 1
+        buy_money += average
+
+
+print(buy_number, buy_money)
